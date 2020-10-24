0d07877f2eb845a694a783449b919f1b2cd9ee63
orlovskiy-artem <artem.orlovsky@gmail.com>
2020-10-24 20:00:40 +0300

    Delete unnecessary files

--- course_progress_service/start_course_progress.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME='/usr/lib/jvm/java-11-openjdk-amd64'
-mvn clean install -DskipTests=true \
-    && docker-compose build \
-    && docker image push artiseasy/course-progress-service:latest \
-    && kubectl create -f course-progress-deployment.yaml,course-progress-service.yaml
